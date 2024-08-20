ac72e5b4d7a4f5f419e1d1a8f286d0dafc3d387a
Chimtamreddy <kr7348202@gmail.com>
2024-08-20 18:54:37 +0530

    Terraform

--- vars.tf
+++ vars.tf
@@ -1,15 +1,15 @@
-# variable "tags" {}
-# variable "env" {}
-# variable "elasticache_type" {}
-# variable "port" {}
-# variable "subnet_ids" {}
-# variable "vpc_id" {}
-# variable "sg_ingress_cidr" {}
-# variable "family" {}
-# variable "engine" {}
-# variable "node_type" {}
-# variable "num_cache_nodes" {}
-# variable "engine_version" {}
-#
-#
-#
+variable "tags" {}
+variable "env" {}
+variable "elasticache_type" {}
+variable "port" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
+variable "family" {}
+variable "engine" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "engine_version" {}
+
+
+
